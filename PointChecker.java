88e676b088d30ee9f0a12d059538cf80acf35705
Matchu391 <124427597+Matchu391@users.noreply.github.com>
2023-02-10 20:44:11 -0800

    Add files via upload

--- PointChecker.java
+++ PointChecker.java
@@ -5,6 +5,7 @@ public class PointChecker {
     int total=0;
     int match=0;
     int points=0;
+  //every thing you need to keep track of
     int spade=0;
     int dia=0;
     int heart=0;
@@ -13,19 +14,16 @@ public class PointChecker {
     int threeOf=0;
     int fourOf=0;
     int straight=0;
+  //used for calculating straight later
     int [] sorts=new int[7];
     int hrow=0;
     int row=0;
     
-    
+    //empty constructor
     public PointChecker(){
-        
-        
-        
-        
-        
+
     }
-    
+    //places the hand you are checking in an array with the table your checking
     public void setup(Table t, Hand h){
         total=t.total+h.total;
         for(int i=0; i<t.total+2; i++){
@@ -33,120 +31,82 @@ public class PointChecker {
                 cards[i]=h.hand[i];
             }else{
                 cards[i]=t.table[i-2];
-                
-                
             }
-            
         }
-        for(int i=0; i<t.total+h.total; i++){
-            //System.out.println("PC: "+cards[i]);
-            
-        }
-        
-        //System.out.println("total cards in pointchecker is: " + total);
-        
-        //checking starts here
         check();
-        //checking ends here
-        //System.out.println("spades: " +spade + ", dia: "+ dia+ ", heart: "+ heart+", clubs: "+ club );
-        //System.out.println("twoOf: "+ twoOf+ ", threeOf: "+ threeOf + ", fourOf: " + fourOf+ ", straight: "+straight);
-        
     }
     
     public void check(){
+      //resets variables in case ran multiple times
+        spade=0;
+        dia=0;
+        heart=0;
+        club=0;
+        twoOf=0;
+        threeOf=0;
+        fourOf=0;
+        straight=0;
     //checks for every suit and checks how many there are   
        for(int i=0; i<total; i++){
             //  23456789TJQKA
             //  CDHS
-            
             if(cards[i].suit=='S'){
-                spade+=1;
-                    
+                spade+=1;  
             }
             if(cards[i].suit=='D'){
-                dia+=1;
-                    
+                dia+=1;  
             }
             if(cards[i].suit=='H'){
-                heart+=1;
-                    
+                heart+=1; 
             }
             if(cards[i].suit=='C'){
-                club+=1;
-                    
+                club+=1;  
             }
         }
         int match=0;
         //checks for how many matches of cards there are
         for(int k=2; k<16; k++){
             if(match==4){
-              
                 fourOf++;
             }
             if(match==3){
-                
                 threeOf++;
-                
             }
-            if(match==2){//doesnt consider aces or kings a pair
+            if(match==2){
                 twoOf++;
-
             }
             
             match=0;
             for(int i=0; i<total; i++){
                 if(k==cards[i].cardValue){
                     match+=1;
-                    
                 }
             }
         }
-        
-        
     //     //checks for if there is a straight
-        
         sort();
+      //goes through and every time point value increases by 1 adds one to row
         for(int i=0; i< sorts.length-1; i++){
-            //System.out.println(sorts[i+1]-sorts[i]);
-            
-            
             if(sorts[i+1]-sorts[i]==1){
                 row++;
-                //System.out.println(row);
+   
                 if(row>hrow){
-                    //System.out.println(hrow);
                     hrow=row;
                 }
             }
-            
-            // if(sorts[i+1]-sorts[i]!= 0 || sorts[i+1]-sorts[i]!= 1){
-            //     row=0;
-            // }
+          //if 5 numbers were in a row it becomes a sraight
             if(row>=5){
                 straight+=1;
             }
         }    
     }   
     
-    
-    
-    
-    
+    //in order to check if there is a straight first you must create an int array of the cards actual point values and use the built in method for sorting them highest to lowest
     public void sort(){
         
-        for(int i=0; i<7; i++){
+        for(int i=0; i<total; i++){
             sorts[i]=cards[i].cardValue;
         }
-        Arrays.sort(sorts);
-        
-        //print
-        // for(int i=0; i< 7; i++){
-        //     System.out.println(sorts[i]);
-        // }
-       
-        
+        Arrays.sort(sorts); 
     }
-    
-    
-    
 }
