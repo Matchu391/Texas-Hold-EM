a2b1d55eb2629b27ad9e9157ec5e8b368660b448
Matchu391 <124427597+Matchu391@users.noreply.github.com>
2023-02-10 20:44:24 -0800

    Add files via upload

--- Hand.java
+++ Hand.java
@@ -7,32 +7,22 @@ public class Hand {
     Card [] hand = new Card[2];
     int total=0;
     
-    
+    //prints the cards int the hand and the current money and betting
     public void rHand(){
+        System.out.println("Total Money: " + money + " Betting: " + betting);
         System.out.println("Hand:");
         for(int i=0; i<total; i++){
             System.out.print(hand[i].toString() + " ");
-            
-            
-            
         }
         System.out.println("");
         System.out.println("");
-        
-        
-        
     }
     
     //Draws a card taking the deck your drawing from as a parameter
     public void draw(Deck c){
         total+=1;
         hand[total-1]=c.deck[0];
+      //deletes the card from the deck after its added to the hand
         c.remove();
-        
-        
     }
-    
-    
-    
-    
 }
